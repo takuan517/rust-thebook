0a3d8fd635dbe929db17cb8826fb734c3d7562f3
takuan517 <ishitaku-2000517@keio.jp>
2021-07-02 20:51:56 +0900

    cariables;

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/ishiharatakumi/develop/rust-thebook/variables/target/debug/deps/variables: src/main.rs
+
+/Users/ishiharatakumi/develop/rust-thebook/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/ishiharatakumi/develop/rust-thebook/variables/target/debug/variables: /Users/ishiharatakumi/develop/rust-thebook/variables/src/main.rs
